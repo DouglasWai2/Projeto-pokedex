2e512b3e751e5976f659aa1882325eef13a278cc
Douglas <douglas.wai@outlook.com>
2023-08-20 22:54:15 -0300

    corner curves and getpokemon function

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,6 @@ async function getPokemon(){
 const response = await fetch ("https://pokeapi.co/api/v2/pokemon-form/1/");
 const pokemons = await response.json();
 console.log(pokemons);
+const pokemonName = pokemons.name
+console.log(pokemonName)
 }
